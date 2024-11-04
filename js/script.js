6e023ddec2c53d473b8e45c1b4faeb835eff4028
rabi_b <rabiatoub@gmail.com>
2024-11-04 16:20:46 -0500

    Re-commit previous changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,9 @@
 const randomFolks = document.querySelector(".random-peeps");
-<<<<<<< HEAD
+
 const selectUserNumber = document.querySelector(".num-users")
 
 const getData = async function(numUsers) {
     const usersRequest = await fetch(`https://randomuser.me/api/?results=${numUsers}`); //use backticks for template literals
-=======
-
-const getData = async function() {
-    const usersRequest = await fetch("https://randomuser.me/api/?results=5");
->>>>>>> a40f79329003f999edfb131e18a7f5e723f944ef
     const data = await usersRequest.json();
     //console.log(data) //check what data is returned
     const userResults = data.results;
@@ -28,13 +23,11 @@ const displayUsers = function (userResults) {
     };
 }
 
-<<<<<<< HEAD
-getData(1)
 
 selectUserNumber.addEventListener("change", function(e) {
     let numUsers = e.target.value
     getData(numUsers)
 })
-=======
+
 getData()
->>>>>>> a40f79329003f999edfb131e18a7f5e723f944ef
+
